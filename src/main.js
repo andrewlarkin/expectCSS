341691d39fc1a894f61c43190360b1a0031a9091
Andrew Larkin <andrewlarkin2@gmail.com>
2012-05-25 22:13:15 -0400

    Added some additional text to the main reporting

--- src/main.js
+++ src/main.js
@@ -6,8 +6,13 @@ requirejs.config({
 });
 
 requirejs(['runner', 'observer'], function(runner, observer){
+    console.log('-----------------------------------------');
+    console.log('Expect CSS - CSS expectation validation\n');
+    console.log('-----------------------------------------');
 
     runner(process.argv[2]);
+
+    console.log('\n-----------------------------------------\n');
 
     //load spec based on args passed into script call (proccess.argv)
 });
